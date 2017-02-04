4a8d5d188493cb00a11b17c753f08f44449378ee
Daniel Bidulock <daniel@capitolhill.ca>
2017-02-04 00:46:15 -0500

    Get basic post model and tests

--- db/migrate/002_create_posts.rb
+++ db/migrate/002_create_posts.rb
@@ -2,10 +2,10 @@ class CreatePosts < ActiveRecord::Migration
   def self.up
     create_table :posts do |t|
       t.string :url
-      t.string :initials
-      t.boolean :approved
-      t.integer :yeses
-      t.integer :nos
+      t.string :tag
+      t.boolean :approved, default: false
+      t.integer :yeses, default: 0
+      t.integer :nos, default: 0
       t.timestamps
     end
   end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,26 +2,37 @@ require 'spec_helper'
 
 RSpec.describe Post, type: :model do
   context 'database schema' do
-#    subject { Post }
     describe 'columns' do
       it { should have_db_column(:id) }
       it { should have_db_column(:url) }
-#      it { should have_property(:initials) }
-#      it { should have_property(:approved) }
-#      it { should have_property(:yeses) }
-#      it { should have_property(:nos) }
-#      it { should have_property(:created_at) }
-#      it { should have_property(:updated_at) }
+      it { should have_db_column(:tag) }
+      it { should have_db_column(:approved) }
+      it { should have_db_column(:yeses) }
+      it { should have_db_column(:nos) }
+      it { should have_db_column(:created_at) }
+      it { should have_db_column(:updated_at) }
     end
   end
 
   context 'validations' do
-#    subject { Post }
-    it { should validate_presence_of (:url) }
-    it { should validate_uniqueness_of (:url) }
-#    it { should validate_presence_of(:description) }
-#    it { should have_many(:performances) }
+    it { should validate_presence_of(:url) }
+    it { should validate_uniqueness_of(:url) }
+    it { should validate_presence_of(:tag) }
+    it { should validate_length_of(:tag).is_at_most(3).on(:create) }
   end
 
+  context 'initialization' do
+    before(:each) do
+      @post = Post.create({ url: 'http://example.com/pic.jpg', tag: 'ldb' })
+    end
+
+    it 'sets yeses to 0' do
+      expect(@post.yeses).to eq(0)
+    end
+
+    it 'sets nos to 0' do
+      expect(@post.nos).to eq(0)
+    end
+  end
 
 end
