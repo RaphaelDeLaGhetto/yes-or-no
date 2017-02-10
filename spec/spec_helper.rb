4c00ac1a77578e5a8278c46113a3daed42e31054
Daniel Bidulock <daniel@capitolhill.ca>
2017-02-10 00:22:45 -0500

    Configure database_cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,71 @@
 RACK_ENV = 'test' unless defined?(RACK_ENV)
 require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
 Dir[File.expand_path(File.dirname(__FILE__) + "/../app/helpers/**/*.rb")].each(&method(:require))
+require 'capybara/rspec'
 require 'shoulda/matchers'
 
 RSpec.configure do |conf|
   conf.include Rack::Test::Methods
   conf.include(Shoulda::Matchers::ActiveModel, type: :model)
   conf.include(Shoulda::Matchers::ActiveRecord, type: :model)
+
+  # database_cleaner
+  conf.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  conf.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+##  conf.use_transactional_fixtures = false
+##
+##  conf.before(:suite) do
+##    if conf.use_transactional_fixtures?
+##      raise(<<-MSG)
+##        Delete line `conf.use_transactional_fixtures = true` from rails_helper.rb
+##        (or set it to false) to prevent uncommitted transactions being used in
+##        JavaScript-dependent specs.
+##
+##        During testing, the app-under-test that the browser driver connects to
+##        uses a different database connection to the database connection used by
+##        the spec. The app's database connection would not be able to access
+##        uncommitted transaction data setup over the spec's database connection.
+##      MSG
+##    end
+##    DatabaseCleaner.clean_with(:truncation)
+##  end
+#
+#  conf.before(:each) do
+#    DatabaseCleaner.strategy = :transaction
+#  end
+#
+#  conf.before(:each, type: :feature) do
+#    # :rack_test driver's Rack app under test shares database connection
+#    # with the specs, so continue to use transaction strategy for speed.
+#    driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
+#
+#    if !driver_shares_db_connection_with_specs
+#      # Driver is probably for an external browser with an app
+#      # under test that does *not* share a database connection with the
+#      # specs, so use truncation strategy.
+#      DatabaseCleaner.strategy = :truncation
+#    end
+#  end
+#
+#  conf.before(:each) do
+#    DatabaseCleaner.start
+#  end
+#
+#  conf.append_after(:each) do
+#    DatabaseCleaner.clean
+#  end
+
+  # Capybara
+  conf.include Capybara::DSL
 end
 
 # You can use this method to custom specify a Rack app
@@ -22,4 +81,7 @@ def app(app = nil, &blk)
   @app ||= block_given? ? app.instance_eval(&blk) : app
   @app ||= Padrino.application
 end
+
+# Capybara
+Capybara.app = app
 
