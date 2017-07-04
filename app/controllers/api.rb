13f4cf72b8d37c9aeb5664dc01188f4fa7c17ca7
Daniel Bidulock <daniel@capitolhill.ca>
2017-07-04 14:57:22 -0400

    Fix hard-coded HMAC key in API

--- app/controllers/api.rb
+++ app/controllers/api.rb
@@ -1,9 +1,12 @@
 YesOrNo::App.controllers :api, :provides => [:json] do
 
+  #
+  # authenticate
+  #
   post :auth, map: "/api/auth" do
     agent = Agent.find_by_email(params[:email])
     if agent && agent.password == params[:password]
-      hmac_secret = 'my$ecretK3y'
+      hmac_secret = ENV['HMAC_SECRET']
       response.status = 200
       JWT.encode({agent_id: agent.id}, hmac_secret, 'HS256')
     else
@@ -32,35 +35,9 @@ YesOrNo::App.controllers :api, :provides => [:json] do
 
     if post.save
       { message: approved ? 'Image submitted successfully' : 'Image submitted for review' }.to_json
-#      flash[:success] = approved ? 'Image submitted successfully' : 'Image submitted for review'
-#      redirect "/post/#{@post.id}"
     else
       response.status = 400
       { error: post.errors.full_messages.map { |msg| "#{msg}" }.join(". ") }.to_json
-#      flash[:error] = @post.errors.full_messages.map { |msg| "#{msg}" }.join(". ")
-#      redirect "/agents/#{@agent.id}/posts"
     end
   end
-
- 
-  # get :index, :map => '/foo/bar' do
-  #   session[:foo] = 'bar'
-  #   render 'index'
-  # end
-
-  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   "Maps to url '/foo/#{params[:id]}'"
-  # end
-
-  # get '/example' do
-  #   'Hello world!'
-  # end
-  
-
 end
